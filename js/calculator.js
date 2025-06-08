5cc3a6dcd251bc7993a4db8e8a2b4438f0a2af82
Angela <angdkay@gmail.com>
2025-06-08 14:29:45 -0600

    Add = button functionality

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,7 @@
 const numButtons = document.querySelectorAll(".num-button");
-const operButtons = document.querySelectorAll(".oper-button")
+const operButtons = document.querySelectorAll(".oper-button");
+const equalButton = document.querySelector(".equal-button");
+
 let nums = [];
 let num = "";
 let calcScreen = "";
@@ -9,35 +11,32 @@ numButtons.forEach(button => {
   button.addEventListener('click', () => {
     showOnScreen(button.innerText);
   });
-})
+});
 
 operButtons.forEach(button => {
   button.addEventListener('click', () => {
-
-    if (num) {
-      nums.push(num);
-    }
-    console.log(`nums outside if statement: ${nums}`);
-    num = ""
-    console.log(nums);
-    if (nums.length == 2) {
-      console.log(`nums length: ${nums.length}`);
-      console.log(`nums inside if statement: ${nums}`);
-      console.log("YEEHAW")
-      console.log(oper);
-      num1 = parseInt(nums[0]);
-      num2 = parseInt(nums[1]);
-      console.log(num1)
-      console.log(num2)
-      calculate(num1, num2);
-      showOnScreen("");
-      console.log(`nums after calculate: ${nums}`);
-    }
+    checkCalculate();
     showOnScreen(button.innerText);
-
     oper = button.innerText;
   });
-})
+});
+
+equalButton.addEventListener('click', () => {
+  checkCalculate();
+  showOnScreen("=");
+});
+
+function checkCalculate() {
+  if (num) {
+      nums.push(num);
+  }
+  num = ""
+  if (nums.length == 2) {
+      num1 = parseInt(nums[0]);
+      num2 = parseInt(nums[1]);
+      calculate(num1, num2);
+  }
+}
 
 function showOnScreen(val) {
   let screen = document.querySelector(".js-calc-screen");
@@ -45,16 +44,12 @@ function showOnScreen(val) {
     num += val;
     calcScreen += val;
   } else {
-    console.log("HATE IT HERE: " + calcScreen[calcScreen.length-1]);
-    if (!parseInt(calcScreen[calcScreen.length-1])) {
-      console.log("HELLO?")
+    if (!parseInt(calcScreen[calcScreen.length-1]) && val != "=") {
       calcScreen = calcScreen.slice(0, calcScreen.length-1) + val;
-      console.log("PLEASE" + calcScreen)
     } else {
       calcScreen += val;
     }
   }
-
   screen.innerText = calcScreen;
 }
 
@@ -69,7 +64,6 @@ function calculate(first, second) {
     num = first / second;
   }
   nums = [num];
-  console.log(nums)
   calcScreen = String(num);
   num = "";
 }
