06b171972f9c0a32e1bd990d1d37f03d3f2685e5
Angela <angdkay@gmail.com>
2025-06-08 15:32:18 -0600

    Add clear and back functionality

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,7 @@
 const numButtons = document.querySelectorAll(".num-button");
 const operButtons = document.querySelectorAll(".oper-button");
 const equalButton = document.querySelector(".equal-button");
+const commandButtons = document.querySelectorAll(".command-button");
 
 let nums = [];
 let num = "";
@@ -23,34 +24,57 @@ operButtons.forEach(button => {
 
 equalButton.addEventListener('click', () => {
   checkCalculate();
-  showOnScreen("=");
+  showOnScreen("");
 });
+
+commandButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    clearOrBackCalculation(button.innerText);
+  })
+})
+
+function showOnScreen(val) {
+  let screen = document.querySelector(".js-calc-screen");
+
+  // For Clear and Back Cases
+  if (val == "") {
+    screen.innerText = calcScreen;
+    return;
+  }
+
+  // For Calculation Cases
+  if (!isNaN(parseInt(val))) {
+    num += val;
+    calcScreen += val;
+  } else {
+    console.log(calcScreen);
+    console.log(val);
+    if (isNaN(parseInt(calcScreen[calcScreen.length - 1])) && val != "=") {
+      console.log("WHATTT")
+      calcScreen = calcScreen.slice(0, calcScreen.length - 1) + val;
+    } else if (val != "=") {
+      calcScreen += val;
+    }
+  }
+  console.log("HEY " + calcScreen);
+  screen.innerText = calcScreen;
+}
 
 function checkCalculate() {
   if (num) {
       nums.push(num);
   }
+
+  console.log("AFTER PUSH")
+  console.log(nums);
   num = ""
   if (nums.length == 2) {
       num1 = parseInt(nums[0]);
       num2 = parseInt(nums[1]);
       calculate(num1, num2);
   }
-}
-
-function showOnScreen(val) {
-  let screen = document.querySelector(".js-calc-screen");
-  if (parseInt(val)) {
-    num += val;
-    calcScreen += val;
-  } else {
-    if (!parseInt(calcScreen[calcScreen.length-1]) && val != "=") {
-      calcScreen = calcScreen.slice(0, calcScreen.length-1) + val;
-    } else {
-      calcScreen += val;
-    }
-  }
-  screen.innerText = calcScreen;
+  console.log("CHECK CALCULATE");
+  console.log(nums);
 }
 
 function calculate(first, second) {
@@ -62,8 +86,44 @@ function calculate(first, second) {
     num = first * second;
   } else if (oper == "/") {
     num = first / second;
+    console.log(`DIVISION: ${first} + ${second}  = ${num}`);
   }
-  nums = [num];
+  nums = [String(num)];
   calcScreen = String(num);
   num = "";
+}
+
+function clearOrBackCalculation(command) {
+  if (num) {
+      nums.push(num);
+  }
+  num = ""
+
+  if (command === "Clear") {
+    calcScreen = "";
+    nums = [];
+
+  } else if (command === "Back") {
+    lastVal = calcScreen[calcScreen.length - 1];
+
+    if (!parseInt(lastVal)) {
+      oper = "";
+    } else {
+      let numString = nums[nums.length - 1];
+      console.log(`BEFORE`);
+      console.log(nums);
+      if (numString && numString.length == 1) {
+        nums.pop();
+      } else if (numString) {
+        num = numString.slice(0, numString.length - 1);
+        nums.pop();
+      }
+    }
+    console.log("BEFORE calc screen: " + calcScreen)
+    calcScreen = calcScreen.slice(0, calcScreen.length - 1);
+    console.log("AFTER calc screen: " + calcScreen)
+  }
+  console.log(`AFTER`);
+  console.log(nums);
+  showOnScreen("");
 }
